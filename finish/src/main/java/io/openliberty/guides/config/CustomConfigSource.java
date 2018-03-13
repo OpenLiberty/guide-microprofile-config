0c54d51de26dd1716b2f2d9d83b63eac3a26aa38
Junyi Sun <junyi.sun@mail.utoronto.ca>
2018-03-13 18:14:19 -0400

    move resources back to start/ and finish/

--- finish/src/main/java/io/openliberty/guides/config/CustomConfigSource.java
+++ finish/src/main/java/io/openliberty/guides/config/CustomConfigSource.java
@@ -28,7 +28,7 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
 public class CustomConfigSource implements ConfigSource {
 
   String fileLocation = System.getProperty("user.dir").split("target")[0]
-      + "../resources/CustomConfigSource.json";
+      + "resources/CustomConfigSource.json";
 
   @Override
   public int getOrdinal() {
