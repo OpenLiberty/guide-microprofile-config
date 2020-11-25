0a42a9999ddf840ac25b7ed2a09a31026fd739ef
Austin Seto <austin.seto@ibm.com>
2020-11-25 11:04:05 -0500

    Script fix

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -5,11 +5,11 @@ do
         t) DATE="${OPTARG}";;
         d) DRIVER="${OPTARG}";;
         b) BUILD="${OPTARG}";;
-        u) DOCKER_USERNAME="${OPTARG}"
+        u) DOCKER_USERNAME="${OPTARG}";;
     esac
 done
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install>" pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" pom.xml
 cat pom.xml
 
 ../scripts/testApp.sh
