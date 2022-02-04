8f8413ad6c259ea7412275ef5555d58612e9b0ba
Gilbert Kwan <gkwan@ca.ibm.com>
2022-02-04 12:25:25 -0500

    update versions

--- finish/src/main/java/io/openliberty/guides/config/CustomConfigSource.java
+++ finish/src/main/java/io/openliberty/guides/config/CustomConfigSource.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2019 IBM Corporation and others.
+ * Copyright (c) 2017, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,9 +13,9 @@
 // tag::customConfig[]
 package io.openliberty.guides.config;
 
-import javax.json.stream.JsonParser;
-import javax.json.stream.JsonParser.Event;
-import javax.json.Json;
+import jakarta.json.stream.JsonParser;
+import jakarta.json.stream.JsonParser.Event;
+import jakarta.json.Json;
 import java.math.BigDecimal;
 import java.util.*;
 import java.io.StringReader;
