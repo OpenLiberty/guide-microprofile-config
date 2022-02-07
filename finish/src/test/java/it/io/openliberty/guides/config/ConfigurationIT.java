9536916ddb02e39e682ad8fc8cc15d78df7db8f2
t27gupta <tushar.gupta@uwaterloo.ca>
2022-02-07 10:24:59 -0500

    configurationIT fix

--- finish/src/test/java/it/io/openliberty/guides/config/ConfigurationIT.java
+++ finish/src/test/java/it/io/openliberty/guides/config/ConfigurationIT.java
@@ -75,8 +75,8 @@ public class ConfigurationIT {
       assertEquals(expected, actual);
     } else {
       assertEquals(
-        "{ \"error\" : \"Service is currently in maintenance.
-        Contact: admin@guides.openliberty.io\" }",
+         "{ \"error\" : \"Service is currently in maintenance."
+         + "Contact: admin@guides.openliberty.io\" }",
           ConfigITUtil.getStringFromURL(client, baseUrl + INVENTORY_HOSTS),
           "The Inventory Service should be in maintenance");
     }
@@ -98,8 +98,8 @@ public class ConfigurationIT {
     String error = ConfigITUtil.getStringFromURL(client, baseUrl + INVENTORY_HOSTS);
 
     assertEquals(
-      "{ \"error\" : \"Service is currently in maintenance.
-      Contact: admin@guides.openliberty.io\" }",
+       "{ \"error\" : \"Service is currently in maintenance."
+       + "Contact: admin@guides.openliberty.io\" }",
         error, "The inventory service should be down in the end");
   }
   // end::testPutServiceInMaintenance[]
@@ -113,8 +113,8 @@ public class ConfigurationIT {
     String error = ConfigITUtil.getStringFromURL(client, baseUrl + INVENTORY_HOSTS);
 
     assertEquals(
-      "{ \"error\" : \"Service is currently in maintenance.
-      Contact: admin@guides.openliberty.io\" }",
+       "{ \"error\" : \"Service is currently in maintenance."
+       + "Contact: admin@guides.openliberty.io\" }",
         error, "The email should be admin@guides.openliberty.io in the beginning");
 
     ConfigITUtil.changeEmail(CUSTOM_CONFIG_FILE, "service@guides.openliberty.io");
@@ -122,8 +122,8 @@ public class ConfigurationIT {
     error = ConfigITUtil.getStringFromURL(client, baseUrl + INVENTORY_HOSTS);
 
     assertEquals(
-      "{ \"error\" : \"Service is currently in maintenance.
-      Contact: service@guides.openliberty.io\" }",
+       "{ \"error\" : \"Service is currently in maintenance."
+       + "Contact: service@guides.openliberty.io\" }",
         error, "The email should be service@guides.openliberty.io in the beginning");
   }
   // end::testChangeEmail[]
