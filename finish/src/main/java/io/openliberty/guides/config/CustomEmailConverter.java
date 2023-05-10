f6438906878be9666037f34264f70017b7cd39d2
Gilbert Kwan <gkwan@ca.ibm.com>
2023-05-10 15:54:00 -0400

    Update to MP6 and JEE10 (#210)
    * update versions
    
    * update version
    
    * adding -ntp to maven (#205)
    
    * update version
    
    * fix superlinter error
    
    * Update CustomConfig.java
    
    * Fix warning
    
    - [WARNING ] RESTEASY002160: Provider instance io.openliberty.guides.inventory.client.UnknownUrlExceptionMapper$Proxy$_$$_WeldClientProxy is already registered.  2nd registration is being ignored.
     - The UnknownUrlExceptionMapper is being registered twice, in SystemClient and in InventoryUtils
    
    * Fix in start directory
    
    ---------
    
    Co-authored-by: jakub-pomykala <56261244+jakub-pomykala@users.noreply.github.com>
    Co-authored-by: Morgan Chang <shin19991207@gmail.com>

--- finish/src/main/java/io/openliberty/guides/config/CustomEmailConverter.java
+++ finish/src/main/java/io/openliberty/guides/config/CustomEmailConverter.java
@@ -1,19 +1,17 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2019 IBM Corporation and others.
+ * Copyright (c) 2017, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-2.0/
  *
- * Contributors:
- *     IBM Corporation - Initial implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 // end::copyright[]
 package io.openliberty.guides.config;
 
 import org.eclipse.microprofile.config.spi.Converter;
-import io.openliberty.guides.config.Email;
 
 // tag::customConfig[]
 public class CustomEmailConverter implements Converter<Email> {
